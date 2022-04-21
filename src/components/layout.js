b7b1cc3332d645ee284d8360f848424c87cd803b
Benjamin Poore <benjamin_poore@comcast.net>
2022-04-21 13:23:11 -0700

    add index hero image

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,18 @@
 import * as React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
 import Navbar from "./navbar";
+import {
+  Container
+} from 'react-bulma-components';
 
-export default function layout() {
-  return(
+export default function layout({ children }) {
+  return (
     <div className='layout'>
-      <Navbar/>
+      <Navbar />
+      <Container>
+        {children}
+      </Container>
+
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,21 @@ import Logo from '../components/logo'
 import Nav from '../components/navbar'
 import Layout from '../components/layout'
 import "./mystyles.scss"
+import { StaticImage } from 'gatsby-plugin-image'
+import welcome from "../images/welcome.png"
 // import { StaticImage } from 'gatsby-plugin-image'
 
 
 const IndexPage = () => {
   return (
-    <Layout/>
+    <Layout>
+      <div className='column is-half is-offset-one-quarter'>
+        <img className=''
+          src={welcome}
+          alt="Welcome to Potter's House" />
+      </div>
+
+    </Layout>
   )
 }
 
